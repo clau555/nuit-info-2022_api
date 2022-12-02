d5e2ddeb513988346a0dd42413d729c5f29cff0c
clau555 <lucas.chardonnet@etu.univ-smb.fr>
2022-12-02 04:36:43 +0100

    feat(app): enable Cors for front

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.setGlobalPrefix("api");
+    app.enableCors({ origin: "*" });
 
     ConfigModule.forRoot({
         ignoreEnvFile: true,
